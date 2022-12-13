99035749c600968f75e58488c0d8af00ccef23a9
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-12-13 19:19:01 -0300

    initial

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const THEME = {
+  colors: {
+    Color9fd5d4: '#9FD5D4',
+    Color57b5b4: '#57B5B4',
+    Color0f9594: '#0F9594',
+    Color0b6868: '#0B6868',
+    Color063c3b: '#063C3B',
+    Colorf6fecc: '#F6FECC',
+    Colore8fe81: '#E8FE81',
+    Colord0fc02: '#D0FC02',
+    Color92b001: '#92B001',
+    Color536501: '#536501',
+    Colorf2f2f2: '#F2F2F2',
+    Colore6e6e6: '#E6E6E6',
+    Colorc7c7c7: '#C7C7C7',
+    Colorababab: '#ABABAB',
+    Color666666: '#666666',
+    Color363636: '#363636',
+    Color1a1a1a: '#1A1A1A',
+    Color181817: '#181817',
+  },
+};
